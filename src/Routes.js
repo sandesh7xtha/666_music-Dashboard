0809951153d776dab3da5ddf93af548e59d33efe
sandesh xtha <sandesh7xtha@gmail.com>
2022-04-22 14:23:04 +0545

    updateStockAfterPurchase

--- src/Routes.js
+++ src/Routes.js
@@ -8,19 +8,19 @@ import Contacts from "./component/MainView/Contacts/Contacts";
 import News from "./component/MainView/News/News";
 import Secondhand from "./component/MainView/Secondhand/Secondhand";
 import Navbar from "./component/Navbar/Navbar";
+import { Login } from "./Blog/pages/Login/Login";
 
 export const Routes = () => {
   return (
     <Switch>
-      {/* <Navbar /> */}
       <Route exact path="/admin" component={Dashboard} />
-      {/* <Route path="/Navbar" component={Navbar} /> */}
       <Route path="/admin/advertisement" component={Advertisement} />
       <Route path="/admin/products" component={Products} />
       <Route path="/admin/OrderDetail" component={OrderDetail} />
       <Route path="/admin/contacts" component={Contacts} />
       <Route path="/admin/news" component={News} />
       <Route path="/admin/secondhand" component={Secondhand} />
+      {/* <Route path="/admin/Login" component={Login} /> */}
     </Switch>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-import App from './App';
-
+import App from "./App";
+import { Login } from "./Blog/pages/Login/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin/Login" component={Login} />
+      </Switch>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
